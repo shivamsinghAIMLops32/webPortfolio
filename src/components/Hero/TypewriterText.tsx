b49df1e1b286b7a90c91a759e54e8446839215fe
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2024-12-16 21:21:38 +0530

    some more changes

--- src/components/Hero/TypewriterText.tsx
+++ src/components/Hero/TypewriterText.tsx
@@ -2,14 +2,16 @@ import Typewriter from 'typewriter-effect';
 
 export default function TypewriterText() {
   return (
-    <div className="text-xl md:text-2xl text-gray-600 font-light">
+    <div className="text-xl md:text-2xl text-red-600 font-light">
       <Typewriter
         options={{
           strings: [
             'Full Stack Developer',
             'MERN Stack Expert',
             'Problem Solver',
-            'Tech Enthusiast'
+            'Tech Enthusiast',
+            'WEB Developer',
+            'An Curious Engineer',
           ],
           autoStart: true,
           loop: true,
